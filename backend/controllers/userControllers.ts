c5e7e5e0674e0a321481d99534688404bd262c74
developerVictoria <zubannekoviktoriia@gmail.com>
2024-12-01 23:42:08 +0200

    pr-3

---
+++ backend/controllers/userControllers.ts
@@ -0,0 +1,18 @@
+import { Request, Response }  from 'express';
+
+export const loginUser = (req :Request, res: Response) =>{
+    //login route
+  
+};
+export const logOutUser = (req :Request, res: Response)=>{
+    //logout route with redirect to login page
+   
+};
+export const singupUser = async (req :Request, res: Response)=>{
+    try{
+
+    }catch(error){
+
+    }
+   
+};
--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,29 +1,12 @@
 import { Request, Response, Router }  from 'express';
-
+import {loginUser, logOutUser, singupUser} from '../controllers/userControllers'
 const router = Router();
 
+router.post("/singup", singupUser);
 
-router.post("/singup", (req :Request, res: Response)=>{
-    //singup route to authenticate
-    console.log('singup');
-    res.send({"msg":"singup"});
-});
+router.post("/login", loginUser);
 
-router.get("/", (req :Request, res: Response)=>{
-    //user route to get users
-    console.log('hello')
-    res.send({"msg":"Hello"});
-    
-});
-
-
-router.post("/login", (req :Request, res: Response)=>{
-    //login route
-});
-
-router.get("/logout", (req :Request, res: Response)=>{
-    //logout route with redirect to login page
-});
+router.get("/logout", logOutUser);
 
 
 export default router;
