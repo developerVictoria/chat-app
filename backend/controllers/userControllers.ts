fce4831b4412c5f69b7524a98e742c3f5b5dab53
developerVictoria <zubannekoviktoriia@gmail.com>
2024-12-02 02:39:10 +0200

    pr-6

--- backend/controllers/userControllers.ts
+++ backend/controllers/userControllers.ts
@@ -1,4 +1,5 @@
 import { Request, Response }  from 'express';
+import {generatePasswordHash} from '../helpers/userHelpers';
 import User from "../models/userModel";
 
 export const loginUser = (req :Request, res: Response) =>{
@@ -9,23 +10,25 @@ export const logOutUser = (req :Request, res: Response)=>{
     //logout route with redirect to login page
    
 };
+
 export const singupUser = async (req :Request, res: Response)=>{
     try{
-        const {firstName, lastName, username, password, confirmPassword} = req.body;
+        const {firstName, lastName, username, password} = req.body;
         let profilePic = "";
-        if(password !== confirmPassword) res.status(400).json({error: "Password doesn't match entered confirm password!"});
-        if(!firstName && !lastName) res.status(400).json({error: "First and last name must be provided!"});
+        if(!password || password.length<=8) res.status(400).json({error: "Password must be provided and be more then 8 symbols!"});
+        if(!firstName || !lastName)res.status(400).json({error: "First and last name must be provided!"});
         if(!username)  res.status(400).json({error: "Username must be provided!"});
 
         const user = await User.findOne({username});
         if(user) res.status(400).json({error: "Username already taken!"});
                 
         profilePic = `https://avatar.iran.liara.run/username?username=${firstName}+${lastName}`;
+        let hashedPassword = await generatePasswordHash(password);
 
         const newUser = new User({
             fullName:`${firstName} ${lastName}`,
             username: username,
-            password: password,
+            password: hashedPassword,
             profilePic: profilePic,
         })
 
@@ -37,8 +40,10 @@ export const singupUser = async (req :Request, res: Response)=>{
             username: newUser.username,
             profilePic: newUser.profilePic,
         })
+            
     }catch(error){
         console.error(error);
+        res.status(500).json({error: `Internal server error ${error}`});
     }
    
 };
