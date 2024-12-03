ac1a1b35bbee49a725aafce2b1229ace590fba30
developerVictoria <zubannekoviktoriia@gmail.com>
2024-12-03 20:47:59 +0200

    pr-16

--- backend/middleware/protectRoute.ts
+++ backend/middleware/protectRoute.ts
@@ -1,9 +1,26 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction }  from 'express';
+import User from "../models/userModel";
 
-export const protectRoute = (req:Request, res:Response, next:NextFunction) =>{
+interface MyUserRequest extends Request {
+    user?: any;
+  }
+
+
+export const protectRoute = async (req:MyUserRequest, res:Response, next:NextFunction) =>{
     try{
-        
+        const token = req.cookies.jwt;
+        if(!token) res.status(401).json({error: `Not authorized for the operation ! No token provided`});
+        let userId: jwt.JwtPayload | undefined = undefined;
+        if(process.env.JWT_SECRET)  userId = jwt.verify(token, process.env.JWT_SECRET) as jwt.JwtPayload 
+        if(!userId) res.status(401).json({error: `Not authorized for the operation ! Invalid token`});
+            
+        const user = await User.findById(userId).select("-password");
+        if(!user) res.status(404).json({error: `Not user found!`});
+
+        req.user = user;
+        next();
+         
     }catch(error){
         console.error(error);
         res.status(500).json({error: `Internal server error ${error}`});
--- backend/routes/messedgeRoutes.ts
+++ backend/routes/messedgeRoutes.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router }  from 'express';
 import {sendMessage} from '../controllers/messageControllers';
 import {protectRoute} from "../middleware/protectRoute";
+
 const router = Router();
 
 router.post("/send/:id",protectRoute,sendMessage);
