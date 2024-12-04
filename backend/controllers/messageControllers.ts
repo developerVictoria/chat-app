10f4658ade504cf538a2950e24baa95ba2e02e23
developerVictoria <zubannekoviktoriia@gmail.com>
2024-12-04 23:11:24 +0200

    pr-17

--- backend/controllers/messageControllers.ts
+++ backend/controllers/messageControllers.ts
@@ -1,11 +1,42 @@
 import { Request, Response, Router }  from 'express';
+import Conversation from '../models/conversationModel';
+import Message from '../models/messageModel';
+import mongoose, {ObjectId} from 'mongoose';
+interface MyUserRequest extends Request {
+    user?: any;
+}
 
-export const sendMessage = (req:Request, res:Response)=>{
+export const sendMessage = async (req:MyUserRequest, res:Response)=>{
     console.log("send msg", req.params.id);
     try{
         const {message} = req.body;
-        const {id} = req.params;
-        const senderId = ''
+        const {id:reciverId} = req.params;
+        const senderId = req.user._id;
+        
+        let conversation = await Conversation.findOne({
+            partisipants:  {$in: [senderId, new mongoose.Types.ObjectId(reciverId)] },
+        });
+        
+        
+        if(!conversation){
+            conversation = await Conversation.create({
+                partisipants:[senderId , reciverId]
+            });
+        } 
+        const newMessage = new Message({
+            senderId:senderId,
+            resiverId: new mongoose.Types.ObjectId(reciverId),
+            message:message,
+
+        });
+        await newMessage.save();
+        if(newMessage){
+            conversation.messages.push(newMessage._id);
+            await conversation.save();
+        };
+        console.log(conversation);
+        res.status(200).json(newMessage)
+
     }catch(error){
         console.error(error);
         res.status(500).json({error: `Internal server error ${error}`});
--- backend/controllers/userControllers.ts
+++ backend/controllers/userControllers.ts
@@ -7,8 +7,9 @@ export const loginUser = async (req :Request, res: Response) =>{
     try{
         const {username, password} = req.body;
         const user = await User.findOne({username});
+        console.log(user);
         const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
-
+        console.log(isPasswordCorrect)
         if(!isPasswordCorrect) res.status(400).json({error: "Invalid username or password !"});
         if(!user){
             res.status(400).json({error: "Invalid username or password !"});
@@ -22,9 +23,9 @@ export const loginUser = async (req :Request, res: Response) =>{
             });
         }
 
-    }catch(error){
-        console.error(error);
-        res.status(500).json({error: `Internal server error ${error}`});
+    }catch(error:any){
+        console.log(error);
+        res.status(500).json({error: `userControls: loginUser: Internal server error ${error.message}`});
     }
   
 };
@@ -34,7 +35,7 @@ export const logOutUser = (req :Request, res: Response)=>{
         res.cookie("jwt","", {maxAge:0});
         res.status(200).json({message:"Logout was successfull!"});
     }catch(error){
-        console.error(error);
+        console.log(error);
         res.status(500).json({error: `Internal server error ${error}`});
     }
 };
@@ -43,11 +44,11 @@ export const singupUser = async (req :Request, res: Response)=>{
     try{
         const {firstName, lastName, username, password} = req.body;
         let profilePic = "";
-        if(!password || password.length<=8) res.status(400).json({error: "Password must be provided and be more then 8 symbols!"});
-        if(!firstName || !lastName)res.status(400).json({error: "First and last name must be provided!"});
-        if(!username)  res.status(400).json({error: "Username must be provided!"});
+        if(!password || password.length<=8){ res.status(400).json({error: "Password must be provided and be more then 8 symbols!"}); return } 
+        if(!firstName || !lastName){ res.status(400).json({error: "First and last name must be provided!"}); return  }
+        if(!username){res.status(400).json({error: "Username must be provided!"}); return  }
         const user = await User.findOne({username});
-        if(user) res.status(400).json({error: "Username already taken!"});
+        if(user){ res.status(400).json({error: "Username already taken!"}); return }
                 
         profilePic = `https://avatar.iran.liara.run/username?username=${firstName}+${lastName}`;
         let hashedPassword = await generatePasswordHash(password);
@@ -59,7 +60,10 @@ export const singupUser = async (req :Request, res: Response)=>{
             profilePic: profilePic,
         })
 
-        if(!newUser) res.status(400).json({error: "Invalid user data!"});
+        if(!newUser){
+            res.status(400).json({error: "Invalid user data!"});
+            return;
+        } 
 
         generateTokenAndSetCookies(newUser._id, res);
         await newUser.save();
@@ -72,9 +76,9 @@ export const singupUser = async (req :Request, res: Response)=>{
             profilePic: newUser.profilePic,
         })
             
-    }catch(error){
-        console.error(error);
-        res.status(500).json({error: `Internal server error ${error}`});
+    }catch(error:any){
+        console.log(error);
+        res.status(500).json({error: `userControls: singupUser: Internal server error ${error.message}`});
     }
    
 };
