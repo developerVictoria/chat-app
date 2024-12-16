5a1772559f7741a0550544ab4b956b32189de976
developerVictoria <zubannekoviktoriia@gmail.com>
2024-12-16 23:01:35 +0200

    pr-22

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,12 +1,17 @@
 import { Request, Response, Router }  from 'express';
-import {loginUser, logOutUser, singupUser} from '../controllers/userControllers'
+import {loginUser, logOutUser, singupUser, getUserForSidebar} from '../controllers/userControllers'
+import {protectRoute} from "../middleware/protectRoute";
 const router = Router();
-
+//auth - TODO: separate into separate files
 router.post("/singup", singupUser);
 
 router.post("/login", loginUser);
 
 router.get("/logout", logOutUser);
 
+
+//user routes
+
+router.get("/",protectRoute,getUserForSidebar);
 
 export default router;
